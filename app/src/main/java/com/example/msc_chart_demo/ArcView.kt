8702e4ab9f2e1b630cd86240f56882bf5c075f05
fixelChaeHyun <chra@fixelsoft.com>
2022-05-30 16:22:54 +0900

    - test filling a fan chart area

--- app/src/main/java/com/example/msc_chart_demo/ArcView.kt
+++ app/src/main/java/com/example/msc_chart_demo/ArcView.kt
@@ -1,33 +1,67 @@
 package com.example.msc_chart_demo
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
 
 class ArcView constructor(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
+   val lineColorMiddle = "#FDC6C6"
+   val lineColorSide = "#A8BBFF"
+   val fillColorMiddle = "#FFA833"
+   val fillColorSide = "#33E3FF"
+
    init {
 
    }
 
+   var fillPercentMiddle = 0.10f
+
    override fun onDraw(canvas: Canvas?) {
       super.onDraw(canvas)
-      val pnt = Paint()
-      pnt.strokeWidth = 6f
-      pnt.color = Color.parseColor("#FF0000")
-      pnt.style = Paint.Style.STROKE
 
-      var rect = RectF()
-      rect[200f, 200f, 600f] = 600f
-      canvas!!.drawArc(rect, 270f, 290f, false, pnt)
+      val paintMiddle = Paint()
+      paintMiddle.strokeWidth = 9f
+      paintMiddle.color = Color.parseColor(lineColorMiddle)
+      paintMiddle.style = Paint.Style.STROKE
+      paintMiddle.pathEffect = DashPathEffect(floatArrayOf(20f, 10f), 0f)
 
-      rect = RectF()
-      rect[300f, 700f, 700f] = 1100f
-      canvas!!.drawArc(rect, 0f, 290f, true, pnt)
+      val paintFillMiddle = Paint().apply {
+         color = Color.parseColor(fillColorMiddle)
+         style = Paint.Style.FILL
+      }
+
+
+//      val fillPercentMiddle = 0.01f
+      var offset = 11f
+      var rectMiddle = RectF(100f, 100f, 1000f, 1000f)
+      canvas!!.drawArc(rectMiddle, 240f, 60f, true, paintMiddle)
+      // RectMiddle 에 Padding 을 넣을 수 있는 방법?
+
+      val fillX = 100f * fillPercentMiddle
+      val fillY = 100f * fillPercentMiddle
+      rectMiddle.set(100f + fillX, 100 + fillY, 1000f - fillY, 1000f - fillY)
+      canvas!!.drawArc(rectMiddle, 240f, 60f, true, paintFillMiddle)
+
+      val paintSide = Paint().apply {
+         strokeWidth = 6f
+         color = Color.parseColor(lineColorSide)
+         style = Paint.Style.STROKE
+         pathEffect = DashPathEffect(floatArrayOf(15f, 10f), 0f)
+      }
+
+
+
+      val rectLeft = RectF(100f - offset, 100f + offset - 2, 1000f - offset, 1000f)
+      canvas!!.drawArc(rectLeft, -165f, 45f, true, paintSide)
+      val rectRight = RectF(100f + offset, 100f + offset- 2, 1000f + offset, 1000f)
+      canvas!!.drawArc(rectRight, -60f, 45f, true, paintSide)
+   }
+
+   fun setFillMiddleWeight(weight: Float) {
+      fillPercentMiddle = weight
+      invalidate()
    }
 }
--- app/src/main/java/com/example/msc_chart_demo/DrawArcActivity.kt
+++ app/src/main/java/com/example/msc_chart_demo/DrawArcActivity.kt
@@ -4,17 +4,60 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
 
 class DrawArcActivity : AppCompatActivity() {
 
     lateinit var arcView : ArcView
+    lateinit var seekbar : SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arc_draw)
 
-        arcView = findViewById<ArcView>(R.id.arcView)
+        val listener = object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                Log.d("seekbar", "p1 : $p1, p2 : $p2")
+                var weight: Float = 0f
+                if (p1 == 0) {
+                    weight = 0.01f
+                } else {
+                    weight = p1.toFloat()
+
+                    if (weight == 4f)
+                        weight = 4.3f
+                }
+
+                arcView.setFillMiddleWeight(weight)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        }
+
+        try {
+            arcView = findViewById<ArcView>(R.id.arcView)
+            seekbar = findViewById(R.id.seekbar)
+
+
+            seekbar.setOnSeekBarChangeListener(listener)
+//            arcView.setFillMiddleWeight(2f)
+
+
+        } catch (e:Exception) {
+
+        }
+
+
+
 
 
     }
